4e65d595767b89352159626a9866a9138c937ad2
David Mawdsley <david.mawdsley@manchester.ac.uk>
2018-07-26 10:19:14 +0100

    Add non interactive gapminder plot

--- worked_example/app.R
+++ worked_example/app.R
@@ -36,7 +36,7 @@ ui <- fluidPage(
       
       # Show a plot of the generated distribution
       mainPanel(
-         plotOutput("distPlot")
+         plotOutput("gapminderPlot")
       )
    )
 )
@@ -44,13 +44,10 @@ ui <- fluidPage(
 # Define server logic required to draw a histogram
 server <- function(input, output) {
    
-   output$distPlot <- renderPlot({
-      # generate bins based on input$bins from ui.R
-      x    <- faithful[, 2] 
-      bins <- seq(min(x), max(x), length.out = input$bins + 1)
-      
-      # draw the histogram with the specified number of bins
-      hist(x, breaks = bins, col = 'darkgray', border = 'white')
+   output$gapminderPlot <- renderPlot({
+      gapminder %>% 
+       filter(year == 2000) %>% 
+       produceGapminderPlot()
    })
 }
 
